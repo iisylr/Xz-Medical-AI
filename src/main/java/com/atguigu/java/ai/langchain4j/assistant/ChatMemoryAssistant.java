0cbfc0e288090c752e4b8a9cc4ea2a42768d8be0
Mospcat <1498400454@qq.com>
2026-05-28 18:41:01 +0800

    1.2 ChatMemoryAssistant

---
+++ src/main/java/com/atguigu/java/ai/langchain4j/assistant/ChatMemoryAssistant.java
@@ -0,0 +1,14 @@
+package com.atguigu.java.ai.langchain4j.assistant;
+
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+
+@AiService(
+        wiringMode = AiServiceWiringMode.EXPLICIT,
+        chatModel = "openAiChatModel",
+        chatMemory = "chatMemory"
+)
+public interface ChatMemoryAssistant {
+
+    String chat(String message);
+}
